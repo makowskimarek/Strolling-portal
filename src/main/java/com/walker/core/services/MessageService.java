215c49e367db01ecf3b35db41a7e521b59caaebd
makowskimarek <makow.mark@gmail.com>
2017-08-13 18:26:43 +0200

    Added message service

---
+++ src/main/java/com/walker/core/services/MessageService.java
@@ -0,0 +1,18 @@
+package com.walker.core.services;
+
+import com.walker.DataBaseControl.databaseException.NotFoundException;
+import com.walker.core.entities.*;
+
+import java.util.List;
+
+/**
+ * @author Marek Makowski
+ * @version 1.0
+ */
+public interface MessageService extends SessionService{
+
+    void addUserMessage(UserMessageData userMessageData);
+    List<UserMessageData> getAllUserMessages(int userId) throws NotFoundException;
+    List<UserMessageData> getUserMessages(int userId, int userId2) throws NotFoundException;
+
+}
---
+++ src/main/java/com/walker/core/services/impl/MessageServiceImpl.java
@@ -0,0 +1,54 @@
+package com.walker.core.services.impl;
+
+import com.walker.DataBaseControl.ControlUser;
+import com.walker.DataBaseControl.ControlUserMessage;
+import com.walker.DataBaseControl.databaseException.NotFoundException;
+import com.walker.core.entities.UserMessageData;
+import com.walker.core.services.MessageService;
+
+import java.util.List;
+
+/**
+ * @author Marek Makowski
+ * @version 1.0
+ */
+public class MessageServiceImpl implements MessageService {
+
+    private ControlUserMessage controlUserMessage;
+    private ControlUser controlUser;
+
+    public MessageServiceImpl() {
+        controlUserMessage = new ControlUserMessage();
+        controlUser = new ControlUser();
+    }
+
+    @Override
+    public List<UserMessageData> getAllUserMessages(int userId) throws NotFoundException {
+        try {
+            List<UserMessageData> userMessagesList = controlUserMessage.getAllUserMessages(userId);
+            return userMessagesList;
+        } catch (NotFoundException e) {
+            throw new NotFoundException();
+        }
+    }
+
+    @Override
+    public List<UserMessageData> getUserMessages(int userId, int userId2) throws NotFoundException {
+        try {
+            List<UserMessageData> userMessagesList = controlUserMessage.getUserMessages(userId, userId2);
+            return userMessagesList;
+        } catch (NotFoundException e) {
+            throw new NotFoundException();
+        }
+    }
+
+    @Override
+    public void addUserMessage(UserMessageData userMessageData) {
+        controlUserMessage.addUserMessage(userMessageData);
+    }
+
+    @Override
+    public int getUserIdFromNick(String nick) {
+        return controlUser.getUserID(nick);
+    }
+}
